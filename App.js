20c0fec75e81dc24d379d5c4cd07ba1a5049e9fa
javier ho <tech@craftome.com>
2022-04-12 16:30:18 +0800

    Showing a custom component

--- App.js
+++ App.js
@@ -5,17 +5,17 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 //=> need list out 'relative path' to import the component
-import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen"; //<== import custom components
 //import ButtonExample from "./src/screens/ButtonExample";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: ComponentsScreen
+    Components: ComponentsScreen //<== add custom component to navigator
   //Button: ButtonExample
   },
   {
-    initialRouteName: 'Components',
+    initialRouteName: 'Components', //<== change to the component you want to display
     defaultNavigationOptions: {
       title: "App",
     },
@@ -25,3 +25,8 @@ const navigator = createStackNavigator(
 export default createAppContainer(navigator);
 
 // LEARN 2. Showing a Custom Component
+//Understand how a component appear on a screen
+
+//note If you don'y understand how Navigator works now, it's okay. Later will have one chapter for navigation.
+
+//here just need to know we need to add it to some parent component to make the component component appear on the screen.
