3190da4073fd0e2bb9b215cd7259221d9f63972e
javier ho <tech@craftome.com>
2022-04-12 14:00:37 +0800

    Overview of React Component

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import componentScreen from "./src/screens/componentScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ButtonExample from "./src/screens/ButtonExample";
 
 const navigator = createStackNavigator(
   {
-    Home: componentScreen,
+    //Home: HomeScreen,
+    //Components: ComponentsScreen,
+    Home: ButtonExample
   },
   {
     initialRouteName: "Home",
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-const componentScreen = () => {
-  return <Text style={styles.text}>Component Screen</Text>;
+const HomeScreen = () => {
+  return <Text style={styles.text}>Hello world </Text>;
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    color: 'green'
   },
 });
 
-export default componentScreen;
+export default HomeScreen;
