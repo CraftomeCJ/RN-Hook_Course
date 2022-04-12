3e1681d242d0b9ac7b2625da46924b57f72ca968
javier ho <tech@craftome.com>
2022-04-12 20:48:54 +0800

    FAQs, Intro to JSX & Common Error

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -10,29 +10,54 @@ import {Text, View, StyleSheet, Button} from 'react-native'; //=> react native c
 
 /*
 JSX Component
-  ==> <Text> = for displaying text
+  ==> <Text> = A 'primitive' React element
+    ==> Used to show some basic content on the screen
+    ==> for displaying text to user
     ==> supports nesting, styling & touch handling
+    ==> Any text place outside "<Text>" will result in error
 
-  ==> StyleSheet an almost identical of CSS StyleSheets
+  ==> StyleSheet.create an almost identical of CSS StyleSheets
+  How 'styles' work? ==> The 'StyleSheet.create()' function validates a set of styling rules that we passed into it.
+    ==> Can be use to or pass styling directly into an element
 
-  ==> View = most fundamental component for building UI;
+  ==> <View> = most fundamental component for building UI
+    ==> General purpose element used for grouping other elements or styling
     ==> a container that supports layout with 'Flexbox', 'style', 'some touching handling' & accessibility controls
     ==> designed to be nested inside other views and can have zero to many children of any type
 
-  ==> Button = should render nicely on any platform
+  ==> <Image> = Show an image
+
+  ==> Button = show a button the user can press.
+    ==> give use feedback whenever the user pressed it
+    ==> should render nicely on any platform
     ==> support a minimal level of customization
+
+  ==> <Text>,<View> ==> JSX = a 'dialect' of JS that tells React what content we want to show.
+
+  ==> 'appNavigator' in 'App.js'??? ==>  a tool from a library call React Navigation that is used to show different screens to the user
 */
 
 //note ==> Part 2: create some component => a function that returns some 'JSX'
 //Text component with Nested text
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyleBase}>
+  //create a variable using JSX element
+  const greeting = <Text>'Hello, my Friend!!!'</Text>;
+
+  //then we can show it inside JSX block
+  //{styles.textStyleBase} <== props
+  return (
+  <View>
+    <Text style={styles.textStyleBase}>
     This is the components screen
       <Text style={styles.textStyleInner}>
         and it look great.
         </Text>
-    </Text>;
+      </Text>
+    {greeting}
+  </View>
+  );
 };
+//important remember to wrap multi-line JSX with (parenthesis)
 
 //note ==> create native component ==> a function that returns some 'JSX' with View, Text & Stylesheet
 const ViewBoxesWithColorAndText = () => {
@@ -97,3 +122,9 @@ export default ComponentsScreen;
 //export default ViewBoxesWithColorAndText;
 
 //LEARN 1. We have just create our FIRST COMPONENT file
+
+//important Rules Of JSX
+//we can assemble different JSX elements like normal HTML
+//we configure different JS element using 'props'
+//we can refer to JS variables inside of a JSX block by using curly braces
+//we can assign JSX elements to variable, then show that variable inside of a JSX block
