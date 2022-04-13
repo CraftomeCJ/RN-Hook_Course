1a0942a97c6ba52728704a5f4f97f11ea0dec2eb
javier ho <tech@craftome.com>
2022-04-14 00:41:31 +0800

    Exercise File component boilerplate

---
+++ src/screens/FlatListExercise.tsx
@@ -0,0 +1,22 @@
+//LEARN FlatList Exercise
+//GOAL
+//Make the FlatList scroll vertically again
+//Add a new 'age' property to each of our 'friend' objects
+//print out that age for each element produced by the FlatList
+
+import React from 'react';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+
+const FlatListExercise = () => {
+  return (
+    <View>
+      <Text>FlatListExercise</Text>
+    </View>
+  )
+}
+
+export default FlatListExercise
+
+const styles = StyleSheet.create({})
+
+
