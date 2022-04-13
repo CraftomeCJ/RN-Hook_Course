6cd92e794244f260483fe2df6ae9994df05b38bc
javier ho <tech@craftome.com>
2022-04-13 22:01:41 +0800

    rendering a FlatList with props

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,26 +1,33 @@
 //important default setup (Boilerplate) for component
 import React from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
+//note FlatList
+// A performant interface for 'rendering' => basic, flat list, supporting handy features:
+//read RN documentation for more details
 
 const ListScreen = () => {
+  //below is an array of objects
   const friends = [
-    {name: 'Friend #1', age: 20},
-    {name: 'Friend #2', age: 21},
-    {name: 'Friend #3', age: 22},
-    {name: 'Friend #4', age: 23},
-    {name: 'Friend #5', age: 24},
-    {name: 'Friend #6', age: 25},
-    {name: 'Friend #7', age: 26},
-    {name: 'Friend #8', age: 27},
-    {name: 'Friend #9', age: 28},
-    {name: 'Friend #10', age: 29},
+    {name: 'Friend #1', age: 20, key: '1'},
+    {name: 'Friend #2', age: 21, key: '2'},
+    {name: 'Friend #3', age: 22, key: '3'},
+    {name: 'Friend #4', age: 23, key: '4'},
+    {name: 'Friend #5', age: 24, key: '5'},
+    {name: 'Friend #6', age: 25, key: '6'},
+    {name: 'Friend #7', age: 26, key: '7'},
+    {name: 'Friend #8', age: 27, key: '8'},
+    {name: 'Friend #9', age: 28, key: '9'},
+    {name: 'Friend #10', age: 29, key: '10'},
   ];
+
   return (
   <FlatList         //<== RN component
+  //important props = 'data' ==> a plain array
   data={friends}    //<== pass in a Prop of data
+  //important props = 'renderItem' ==> take an 'item' from 'data' and renders it into the list
   renderItem={({item}) => {  //pass in another Prop with arg
-    // element === {item: {name: 'Friend #1', age: 20}, index: 0}
-    //or item === {name: 'Friend #2', age: 21}
+    // arg element will be like ===> {item: {name: 'Friend #1', age: 20}, index: 0}
+    //while arg item will be like ===> {name: 'Friend #2', age: 21}
 
     return (
     <Text>
