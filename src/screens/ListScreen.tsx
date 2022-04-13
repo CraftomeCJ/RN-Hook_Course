f03fc9d9bcca892cdbedd810bc199ab6258da5c9
javier ho <tech@craftome.com>
2022-04-13 19:44:48 +0800

    The FlatList Element

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -17,3 +17,13 @@ const styles = StyleSheet.create({});
 
 
 export default ListScreen;
+
+//understand what is flatList and how it form the list from the datasource
+
+//LEARN FlatList Element
+/*
+=> Turns an array into a list of elements
+=> we are required to pass in a 'prop" of 'data' ==> the array of data that we are going to create a bunch of elements out of
+=> also required to pass in a 'renderItem' prop ==> function that will turn each individual item into an element
+=> if you are coming from React on the web, I might be used to 'mapping' an array of data to build a list ==> FlatList is better with RN
+*/
