bebd6149c8f738b48f0a4091c21a18ca4828eadb
javier ho <tech@craftome.com>
2022-04-17 13:10:26 +0800

    create imageScreen boilerplate for component reuse with props practice

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
 import ListScreen from "./src/screens/ListScreen";
-//import ImageScreen from "./src/screens/ImageScreen";
+import ImageScreen from "./src/screens/ImageScreen";
 //import Counter from "./src/screens/Counter";
 //import TableComponent from "./src/screens/TableComponent";
 import AnotherListScreen from "./src/screens/AnotherListScreen";
@@ -15,7 +15,7 @@ const navigator = createStackNavigator(
     Home: HomeScreen,
     Components: ComponentsScreen,
     List: ListScreen,
-    //Image: ImageScreen,
+    Image: ImageScreen,
     //Counter: Counter,
     //Table: TableComponent,
     AnotherList: AnotherListScreen,
