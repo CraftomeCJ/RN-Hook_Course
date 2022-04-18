3ce1cdc64442fb5f4bc3d279a8f8cf3f4a8d688a
javier ho <tech@craftome.com>
2022-04-18 13:20:02 +0800

    State in Action & intro to useState

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -5,12 +5,28 @@
 //todo What is the starting value of that piece of data?
 
 import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 
 const CounterScreen = () => {
+  //important Understand how to use setState. Never change the state directly.
+  const [
+    counter, //<== the element you want to track
+    setCounter //<== function to set the counter state
+  ] = useState //<== useState is a hook that is a function that returns an array of two elements, the first element is the value of the counter, the second element is a function to set the counter state
+  (0) //<== (with default value you set)
+
   return (
     <View>
-      <Text style={styles.counterStyle}>CounterCurrent Count:</Text>
+      <Button title='Increase' onPress={() => {
+        setCounter(counter + 1)
+      }}
+        />
+      <Button title='Decrease' onPress={() => {
+        setCounter(counter - 1)
+      }} />
+      <Text style={styles.counterStyle}>
+        Counter Count: {counter}
+        </Text>
     </View>
   )
 }
