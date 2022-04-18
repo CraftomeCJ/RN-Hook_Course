07ac5c24e3506211fb4069fbddf1ef2038b61ff6
javier ho <tech@craftome.com>
2022-04-19 01:35:42 +0800

    add main content & styling

--- src/screens/TextStyleProps.tsx
+++ src/screens/TextStyleProps.tsx
@@ -1,6 +1,7 @@
 
-import { FlatList, Platform, ScrollView, Slider, StatusBar, StyleSheet, Switch, Text, TouchableWithoutFeedback, View } from 'react-native'
+import { FlatList, Platform, ScrollView, StatusBar, StyleSheet, Switch, Text, TouchableWithoutFeedback, View } from 'react-native'
 import React, {useState} from 'react'
+import Slider from '@react-native-community/slider'
 
 const fontStyles = ["normal", "italic", "oblique"]
 const fontVariants = [
@@ -75,12 +76,284 @@ const [fontStyleIdx, setFontStyleIdx] = useState(0);
   });
 
   return (
-    <View>
-      <Text>TextStyleProps</Text>
+    <View style={styles.container}>
+      <Text
+        style={[
+          styles.paragraph,
+          {
+            fontSize,
+            fontStyle: fontStyles[fontStyleIdx],
+            fontWeight: fontWeightIdx[fontWeightIdx],
+            lineHeight,
+            textAlign: textAlignments[textAlignIdx],
+            textDecorationLine: textDecorationLineIdx[textDecorationLineIdx],
+            textTransform: textTransformations[textTransformIdx],
+            textAlignVertical: textAlignmentsVertical[textVerticalAlignIdx],
+            fontVariant: [fontVariants[fontVariantIdx]],
+            letterSpacing,
+            includeFontPadding,
+            textDecorationStyle: textDecorationStyles[textDecorationStyleIdx],
+            writingDirection: writingDirections[writingDirectionIdx],
+            textShadowOffset,
+            textShadowRadius
+          }
+        ]}
+      >
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 112 Likes
+      </Text>
+      <ScrollView>
+        <View>
+          <Text>Common platform properties</Text>
+          <CustomSlider
+            label="Text Shadow Offset - Height"
+            value={textShadowOffset.height}
+            minimumValue={-40}
+            maximumValue={40}
+            handleValueChange={(val: any) =>
+              setTextShadowOffset(prev => ({ ...prev, height: val }))
+            }
+          />
+          <CustomSlider
+            label="Text Shadow Offset - Width"
+            value={textShadowOffset.width}
+            minimumValue={-40}
+            maximumValue={40}
+            handleValueChange={(val: any) =>
+              setTextShadowOffset(prev => ({ ...prev, width: val }))
+            }
+          />
+          <CustomSlider
+            label="Font Size"
+            value={fontSize}
+            maximumValue={40}
+            handleValueChange={setFontSize}
+          />
+          <CustomPicker
+            label="Font Style"
+            data={fontStyles}
+            currentIndex={fontStyleIdx}
+            onSelected={setFontStyleIdx}
+          />
+          <CustomPicker
+            label="Font Weight"
+            data={fontWeightIdx}
+            currentIndex={fontWeightIdx}
+            onSelected={setFontWeightIdx}
+          />
+          <CustomSlider
+            label="Line Height"
+            value={lineHeight}
+            minimumValue={10}
+            maximumValue={50}
+            handleValueChange={setLineHeight}
+          />
+          <CustomPicker
+            label="Text Align"
+            data={textAlignments}
+            currentIndex={textAlignIdx}
+            onSelected={setTextAlignIdx}
+          />
+          <CustomPicker
+            label="Text Decoration Line"
+            data={textDecorationLineIdx}
+            currentIndex={textDecorationLineIdx}
+            onSelected={setTextDecorationLineIdx}
+          />
+          <CustomSlider
+            label="Text Shadow Radius"
+            value={textShadowRadius}
+            handleValueChange={setTextShadowRadius}
+          />
+          <CustomPicker
+            label="Font Variant"
+            data={fontVariants}
+            currentIndex={fontVariantIdx}
+            onSelected={setFontVariantIdx}
+          />
+          <CustomSlider
+            label="Letter Spacing"
+            step={0.1}
+            value={letterSpacing}
+            handleValueChange={setLetterSpacing}
+          />
+          <CustomPicker
+            label="Text Transform"
+            data={textTransformations}
+            currentIndex={textTransformIdx}
+            onSelected={setTextTransformIdx}
+          />
+        </View>
+        {Platform.OS === "android" && (
+          <View style={styles.platformContainer}>
+            <Text style={styles.platformContainerTitle}>
+              Android only properties
+            </Text>
+            <CustomPicker
+              label="Text Vertical Align"
+              data={textAlignmentsVertical}
+              currentIndex={textVerticalAlignIdx}
+              onSelected={setTextVerticalAlignIdx}
+            />
+            <CustomSwitch
+              label="Include Font Padding"
+              handleValueChange={setIncludeFontPadding}
+              value={includeFontPadding}
+            />
+          </View>
+        )}
+        {Platform.OS === "ios" && (
+          <View style={styles.platformContainer}>
+            <Text style={styles.platformContainerTitle}>
+              iOS only properties
+            </Text>
+            <CustomPicker
+              label="Text Decoration Style"
+              data={textDecorationStyles}
+              currentIndex={textDecorationStyleIdx}
+              onSelected={setTextDecorationStyleIdx}
+            />
+            <CustomPicker
+              label="Writing Direction"
+              data={writingDirections}
+              currentIndex={writingDirectionIdx}
+              onSelected={setWritingDirectionIdx}
+            />
+          </View>
+        )}
+      </ScrollView>
     </View>
-  )
+  );
 }
+
+const CustomSwitch = ({ label, handleValueChange, value }:{ label: any, handleValueChange: any, value: any }): any => {
+  return (
+    <>
+      <Text style={styles.title}>{label}</Text>
+      <View style={{ alignItems: "flex-start" }}>
+        <Switch
+          trackColor={{ false: "#767577", true: "#DAA520" }}
+          thumbColor={value ? "#DAA520" : "#f4f3f4"}
+          onValueChange={handleValueChange}
+          value={value}
+        />
+      </View>
+    </>
+  );
+}
+
+const CustomSlider = ({
+  label,
+  handleValueChange,
+  step = 1,
+  minimumValue = 0,
+  maximumValue = 10,
+  value
+}) => {
+  return (
+    <>
+      {label && (
+        <Text style={styles.title}>{`${label} (${value.toFixed(2)})`}</Text>
+      )}
+      <View style={styles.wrapperHorizontal}>
+        <Slider
+          thumbTintColor="#DAA520"
+          minimumTrackTintColor="#DAA520"
+          minimumValue={minimumValue}
+          maximumValue={maximumValue}
+          step={step}
+          onValueChange={handleValueChange}
+          value={value}
+        />
+      </View>
+    </>
+  );
+}
+
+const CustomPicker = ({ label, data, currentIndex, onSelected }:{ label: any, data: any, currentIndex: any, onSelected: any }): any => {
+  return (
+    <>
+      <Text style={styles.title}>{label}</Text>
+      <View style={styles.wrapperHorizontal}>
+        <FlatList
+          bounces
+          horizontal
+          data={data}
+          keyExtractor={(item, idx) => String(item)}
+          renderItem={({ item, index }) => {
+            const selected = index === currentIndex;
+            return (
+              <TouchableWithoutFeedback onPress={() => onSelected(index)}>
+                <View
+                  style={[
+                    styles.itemStyleHorizontal,
+                    selected && styles.itemSelectedStyleHorizontal
+                  ]}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      color: selected ? "black" : "grey",
+                      fontWeight: selected ? "bold" : "normal"
+                    }}
+                  >
+                    {item + ""}
+                  </Text>
+                </View>
+              </TouchableWithoutFeedback>
+            );
+          }}
+        />
+      </View>
+    </>
+  );
+}
+
 
 export default TextStyleProps
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8
+  },
+  paragraph: {
+    color: "black",
+    textDecorationColor: "yellow",
+    textShadowColor: "red",
+    textShadowRadius: 1,
+    margin: 24
+  },
+  wrapperHorizontal: {
+    height: 54,
+    justifyContent: "center",
+    color: "black",
+    marginBottom: 12
+  },
+  itemStyleHorizontal: {
+    marginRight: 10,
+    height: 50,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: "grey",
+    borderRadius: 25,
+    textAlign: "center",
+    justifyContent: "center"
+  },
+  itemSelectedStyleHorizontal: {
+    borderWidth: 2,
+    borderColor: "#DAA520"
+  },
+  platformContainer: {
+    marginTop: 8,
+    borderTopWidth: 1
+  },
+  platformContainerTitle: {
+    marginTop: 8
+  },
+  title: {
+    fontWeight: "bold",
+    marginVertical: 4
+  }
+});
