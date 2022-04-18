145ef089f5037f8010f75ac50b5d9111cac7fdc4
javier ho <tech@craftome.com>
2022-04-19 01:08:19 +0800

    add TextStyleProps component

--- src/screens/TextStyleProps.tsx
+++ src/screens/TextStyleProps.tsx
@@ -1,1 +1,15 @@
 
+import { StyleSheet, Text, View } from 'react-native'
+import React, {useState} from 'react'
+
+const TextStyleProps = () => {
+  return (
+    <View>
+      <Text>TextStyleProps</Text>
+    </View>
+  )
+}
+
+export default TextStyleProps
+
+const styles = StyleSheet.create({})
