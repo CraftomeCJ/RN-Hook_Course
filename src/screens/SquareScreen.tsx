84f54cc7364c6d71881fd225f5fc11d40b76e2b0
javier ho <tech@craftome.com>
2022-04-26 11:36:39 +0800

    minor edit squarescreen file

--- src/screens/SquareScreen.tsx
+++ src/screens/SquareScreen.tsx
@@ -30,9 +30,6 @@ const SquareScreen: React.FC = () => {
   //Convert the states of red, blue, green into a state of object with property of red, blue and green
   //useState is a function that returns an array of two values. The first value is the current state of the variable. The second value is a function that allows you to update the state of the variable.
 
-/*
-//// Creating a Reducer here:
-*/
   const [red, setRed] = useState(180);
   const [green, setGreen] = useState(180);
   const [blue, setBlue] = useState(180);
@@ -142,3 +139,5 @@ const styles = StyleSheet.create({
 2. Convert the states of red, blue, green into a state of object with property of red, blue and green
 3. Try the initial state with null and fix the issue to make it work.
 */
+
+//bug to refactor with typeof keyof
