95e907e12736afd0816dd258f01dd4243ada5b41
javier ho <tech@craftome.com>
2022-04-27 11:21:57 +0800

    community convention in reducer

--- src/screens/ReducerSqScreen.tsx
+++ src/screens/ReducerSqScreen.tsx
@@ -33,20 +33,21 @@ const COLOR_INCREMENT = 5;
       //action ==> use "action:" instead of long name like "howToChange StateObject"
 const ReducerSqScreen: React.FC = () => {
 //note second: define our reducer function
-const reducer = (state: {red: number, green: number, blue: number}, action: {colorToChange: string, amount: number}) => {
+const reducer = (state: {red: number, green: number, blue: number}, action: {type: string, payload: number}) => {
   // state === {red: number, green: number, blue: number};
   //action is an object that contains the color and amount of color to change
   //example action === {colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15}
+  //I can further refactor action === {type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15}
   //action.color is the color to change
   //action.amount is the amount of color to change
-switch (action.colorToChange) {
-  case 'red':
+switch (action.type) {
+  case 'change_red':
     // add Validation
-    return state.red + action.amount > 255 || state.red + action.amount < 0 ? state : {...state, red: state.red + action.amount}
-  case 'green':
-    return state.green + action.amount > 255 || state.green + action.amount < 0 ? state : {...state, green: state.green + action.amount}
-  case 'blue':
-    return state.blue + action.amount > 255 || state.blue + action.amount < 0 ? state : {...state, blue: state.blue + action.amount}
+    return state.red + action.payload > 255 || state.red + action.payload < 0 ? state : {...state, red: state.red + action.payload}
+  case 'change_green':
+    return state.green + action.payload > 255 || state.green + action.payload < 0 ? state : {...state, green: state.green + action.payload}
+  case 'change_blue':
+    return state.blue + action.payload > 255 || state.blue + action.payload < 0 ? state : {...state, blue: state.blue + action.payload}
   default:
     return state;
 }
@@ -60,22 +61,21 @@ const {red, green, blue} = state;
 
   return (
     <View style={styles.container}>
- {/* way 1 <== not the best way is using if else with || && ! */}
         <ColorCounter
-        onIncrease={() => dispatch({colorToChange: 'red', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({colorToChange: 'red', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({type: 'change_red', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({type: 'change_red', payload: -1 * COLOR_INCREMENT})}
 
           color="Red"/>
 
         <ColorCounter
-        onIncrease={() => dispatch({colorToChange: 'green', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({colorToChange: 'green', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({type: 'change_green', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({type: 'change_green', payload: -1 * COLOR_INCREMENT})}
 
         color="Green"/>
 
         <ColorCounter
-        onIncrease={() => dispatch({colorToChange: 'blue', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({colorToChange: 'blue', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({type: 'change_blue', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({type: 'change_blue', payload: -1 * COLOR_INCREMENT})}
 
         color="Blue"/>
 
@@ -111,3 +111,15 @@ const styles = StyleSheet.create({
   },
 })
 
+
+//learn Our Action Object Had..
+/*
+{colorToChange: 'red', amount: 15}
+
+note usually by convention, we'll instead use:
+{type: 'change_red', payload: 15}
+
+type --> String that describes the exact change operation we want to make
+
+payload --> some data that critical to the change operation
+*/
